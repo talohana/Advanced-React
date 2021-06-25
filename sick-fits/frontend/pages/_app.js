80b57f1631ba861f11f91258a5fcb792925db8cb
Tal Ohana <tal.ohana.x@gmail.com>
2021-06-25 15:46:35 +0300

    Module #3

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,5 +1,12 @@
-import React from 'react';
-import { Page } from '../components/Page';
+import Router from "next/router";
+import NProgress from "nprogress";
+import React from "react";
+import { Page } from "../components/Page";
+import "../components/styles/nprogress.css";
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeError", () => NProgress.done());
+Router.events.on("routeChangeComplete", () => NProgress.done());
 
 const MyApp = ({ Component, pageProps }) => (
   <Page>
--- sick-fits/frontend/pages/_document.js
+++ sick-fits/frontend/pages/_document.js
@@ -1,11 +1,25 @@
-import Document, { Html, Main, NextScript } from "next/document";
+import Document, { Head, Html, Main, NextScript } from "next/document";
 import React from "react";
+import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage((App) => (props) =>
+      sheet.collectStyles(<App {...props} />)
+    );
+    const styleTags = sheet.getStyleElement();
+
+    return {
+      ...page,
+      styleTags
+    }
+  }
+
   render() {
     return (
       <Html lang="en-CA">
-        {/* <Head></Head> */}
+        <Head></Head>
         <body>
           <Main />
           <NextScript />
